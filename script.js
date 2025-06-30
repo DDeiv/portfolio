7e77fe8a3c8a3d21d5c78f672c9b1fa0b09c6f21
DDeiv <75395327+DDeiv@users.noreply.github.com>
2025-06-30 15:19:20 +0200

    Update script.js

--- script.js
+++ script.js
@@ -57,7 +57,7 @@ looking sharp. I also dove into solo web design projects like (vivilecanarie.com
 see the preview]. I've collaborated with (corsedimoto.com), working on their website and taking the brand further
 into the world of YouTube. I'm also co-founder and visual designer of (Soup.fm), a cultural project based in Lisbon 
 that made around 4000 people gather in 2024 and it's still going strong. 
-(here) you can see some of my works.
+(Here) you can see some of my works.
 (Contact) me if you want to grab a coffee or a beer and talk about your feelings or even hire me :]`;
 
 function parseText(text) {
